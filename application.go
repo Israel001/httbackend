321cf7ed4688f3699fc672cc405e511ed2e13083
Israel <israelobanijesu2@gmail.com>
2022-05-23 10:40:50 +0100

    change database

--- application.go
+++ application.go
@@ -4,7 +4,7 @@ import (
 	"fmt"
 	"htt/httbackend/config"
 
-	"gorm.io/driver/mysql"
+	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
 )
 
@@ -19,15 +19,8 @@ func (a *ApplicationHandler) PrepareConfig() {
 
 func (a *ApplicationHandler) InitDB() {
 	config := a.Config.Database
-	connectionString := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?charset=utf8&parseTime=True&loc=Local", config.DbUsername, config.DbPassword, config.DbHost, config.DbPort, config.DbName)
-	_, err := gorm.Open(mysql.New(mysql.Config{
-		DSN: connectionString,
-		DefaultStringSize: 256,
-		DisableDatetimePrecision: true,
-		DontSupportRenameIndex: true,
-		DontSupportRenameColumn: true,
-		SkipInitializeWithVersion: false,
-	}), &gorm.Config{})
+	connectionString := fmt.Sprintf("host=%s user=%s password=%s dbname=%s port=%s sslmode=disable TimeZone=Asia/Shanghai", config.DbHost, config.DbUsername, config.DbPassword, config.DbName, config.DbPort)
+	_, err := gorm.Open(postgres.Open(connectionString), &gorm.Config{})
 
 	if err != nil {
 		defer fmt.Printf("DATABASE ERROR: %v\n", err)
