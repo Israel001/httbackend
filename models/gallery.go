194485e19355805f95e0af7096b1b7b2411f4010
Israel <israelobanijesu2@gmail.com>
2022-11-05 19:30:35 +0100

    update

--- models/gallery.go
+++ models/gallery.go
@@ -3,6 +3,6 @@ package models
 type Gallery struct {
 	BaseModel
 	Title string `gorm:"type:varchar(50) not null" json:"title"`
-	Image string `gorm:"type:varchar(50) not null" json:"image"`
+	Image string `gorm:"type:varchar(500) not null" json:"image"`
 	Date  string `gorm:"type:varchar(50) not null" json:"date"`
 }
